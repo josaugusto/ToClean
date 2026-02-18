193bb501f33d6acb5d4ab3b4a5e8198cfd3cb518
José Augusto <augustoreal.santos@gmail.com>
2026-02-18 16:52:58 -0300

    ToClean v0.1

---
+++ main.lua
@@ -0,0 +1,47 @@
+local sys = require "sys"
+local sysutils = require "sysutils"
+local console = require "console"
+
+console.title = "ToClean"
+
+local function emptyRecycleBin()
+    console.writecolor("white", "Emptying Recycle Bin...")
+    sysutils.recyclebin.empty("C:\\")
+end
+
+local function deleteTemporaryFiles()
+    console.writecolor("white", "Cleaning temporary files...")
+    local systemTemp = sys.Directory("C:\\Windows\\Temp")
+    local windowsUpdate = sys.Directory("C:\\Windows\\SoftwareDistribution\\Download")
+    local prefetch = sys.Directory("C:\\Windows\\prefetch")
+    local userTemp = sys.Directory(sysutils.folders.LocalAppData .. "\\temp")
+
+    local tempDirectories = {systemTemp, windowsUpdate, prefetch, userTemp}
+
+    for directories in each(tempDirectories) do
+	    for item in each(directories) do
+            item:remove()
+        end
+    end
+end
+
+local menuChoice = 0
+
+while menuChoice ~= 3 do
+    console.clear()
+    console.writecolor("green", "1 - Empty Recycle Bin\n")
+    console.writecolor("green", "2 - Delete Temporary Files\n")
+    console.writecolor("green", "3 - Exit\n")
+    console.write("-> ") menuChoice = console.readln()
+    
+    if tonumber(menuChoice) == 1 then
+        emptyRecycleBin()
+    elseif tonumber(menuChoice) == 2 then
+        deleteTemporaryFiles()
+    elseif tonumber(menuChoice) == 3 then 
+        sys.exit()
+    else
+        console.writecolor("red", "Invalid choice!")
+    end
+    sleep(3000)
+end
